bd1a1df2e5dcd936df9d9711fe68089980dda828
CodeCreator <wettig.alexander@gmail.com>
2022-04-13 16:46:25 -0400

    wandb config may change (when resuming)

--- fairseq/logging/progress_bar.py
+++ fairseq/logging/progress_bar.py
@@ -510,7 +510,7 @@ class WandBProgressBarWrapper(BaseProgressBar):
     def update_config(self, config):
         """Log latest configuration."""
         if wandb is not None:
-            wandb.config.update(config)
+            wandb.config.update(config, allow_val_change=True)
         self.wrapped_bar.update_config(config)
 
     def _log_to_wandb(self, stats, tag=None, step=None):
